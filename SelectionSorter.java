80bf620646149f1fc957e3c58e473e800360a871
Oliver <ocai00@stuy.edu>
2019-05-07 11:37:44 -0400

    Finished

--- SelectionSorter.java
+++ SelectionSorter.java
@@ -1,9 +1,39 @@
+import java.util.ArrayList;
+
 public class SelectionSorter extends Sorter{
+
 
 	public SelectionSorter(ArrayList<String> usersData){
 	super(usersData);
-	
+	mySort();
 	}
+
+	public void mySort(){
+		ArrayList<String> sortedList = new ArrayList<String>();
+		while (sortedList.size() < elements.size()) { 
+			int nextLargerAt = champIndex( elements);
+			String min = elements.get(nextLargerAt);
+			sortedList.add(min);
+			elements.set(nextLargerAt, null);
+	}
+		elements = sortedList;
+}
+
+	private int champIndex( ArrayList<String> challengers) {
+		int smallestIndex = 0;
+		while(challengers.get(smallestIndex) == null) {
+			smallestIndex++;
+		}
+		String min = challengers.get(smallestIndex);
+		int minIndex = 0;
+		for(int index = 0; index < challengers.size(); index++) {
+			if (challengers.get(index) != null) {
+			if(challengers.get(index).compareTo(min) <= 0) minIndex = index;
+			}
+		}
+		return minIndex;
+				
+		}
 
 
 }
